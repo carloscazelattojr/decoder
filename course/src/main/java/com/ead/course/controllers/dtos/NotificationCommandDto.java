686fb57b86d3faf02ebdbf8dc2abc1601eceb62e
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-07-08 07:55:24 -0300

    adição nova rota gateway

---
+++ course/src/main/java/com/ead/course/controllers/dtos/NotificationCommandDto.java
@@ -0,0 +1,15 @@
+package com.ead.course.controllers.dtos;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class NotificationCommandDto {
+
+    private String title;
+    private String message;
+    private UUID userId;
+
+
+}
